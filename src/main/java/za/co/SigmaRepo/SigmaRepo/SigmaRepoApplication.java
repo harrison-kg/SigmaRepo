22d0bcf55f081d69b267857507ec1b493a92c7f1
harrison-kg <Harris.25>
2019-11-07 10:52:38 +0200

    #initial commit

---
+++ src/main/java/za/co/SigmaRepo/SigmaRepo/SigmaRepoApplication.java
@@ -0,0 +1,13 @@
+package za.co.SigmaRepo.SigmaRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmaRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/SigmaRepo/SigmaRepo/SigmaRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.SigmaRepo.SigmaRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmaRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
